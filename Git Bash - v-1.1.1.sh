50ed43c6215065dbce423d6dab25a5b502da879a
Rz Rasel <emailrzrasel@gmail.com>
2025-04-21 23:38:52 +0600

    Update  20250421 23:38:52

--- Git Bash - v-1.1.1.sh
+++ Git Bash - v-1.1.1.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
-printf "Enter Commit Text "
-read input
 
-dateTime=$(date "+%Y%m%d %H%M%S")
-commit="git commit m"
-gitcommit="$commit \"$input  $dateTime\""
-echo "$gitcommit"
+# Prompt user for commit text
+read -p "Enter Commit Text: " input
+
+# If input is empty, exit
+if [[ -z "$input" ]]; then
+    echo "You must enter a commit message!"
+    exit 1
+fi
+
+# Get current branch name and datetime
+branch=$(git rev-parse --abbrev-ref HEAD)
+dateTime=$(date "+%Y%m%d %H:%M:%S")
+message="$input  $dateTime"
+
+# Show commit info
+echo "Branch: $branch"
+echo "Commit message: $message"
+
+# Run git commands
 git add .
-git commit m "$input  $dateTime"
-git pull
-git push all
-echo Process Complete Press Enter....
+git commit -m "$message"
+git pull --all
+git push --all
+
+# Done
+echo "Process Complete. Press Enter to exit..."
+read
